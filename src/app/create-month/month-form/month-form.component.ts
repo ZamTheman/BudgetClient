582c8c17d5eb68496d9e13a5ffc8d9fe6a9e58db
samuel.wahlberget@gmail.com <samuel.wahlberget@gmail.com>
2018-01-10 21:19:39 +0100

    Changed month selector. Graphical tuning

--- src/app/create-month/month-form/month-form.component.ts
+++ src/app/create-month/month-form/month-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
 import { MonthService } from '../../services/month.service';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-month-form',
@@ -8,17 +9,64 @@ import { MonthService } from '../../services/month.service';
   styleUrls: ['./month-form.component.css']
 })
 export class MonthFormComponent implements OnInit {
-  dateChanged = new Subject<Date>();
-  dt: Date;
+  monthForm: FormGroup;
+  months: string[];
+  years: number[];
 
-  constructor(private monthService: MonthService) { }
+  constructor(private monthService: MonthService) {}
 
   ngOnInit() {
-    this.dt = new Date();
+    this.months = this.createMonths();
+    this.years = this.createYears();
+    this.monthForm = this.createForm();
   }
 
-  onDateChanged($event){
-    this.monthService.activeMonthChanged($event);
+  selectedDateChanged(){
+    this.monthService.activeMonthChanged(new Date(
+      this.monthForm.value['year'],
+      this.getIdForMonth(this.monthForm.value['month']),
+      1
+      ))
   }
 
+  createForm() : FormGroup{
+    return new FormGroup({
+      'year': new FormControl(new Date().getFullYear()),
+      'month': new FormControl(this.months[new Date().getMonth()])
+    })
+  }
+
+  private createYears() : number[]{
+    let years = new Array();
+    for(let i = new Date().getFullYear() - 5; i < new Date().getFullYear() + 3; i++){
+      years.push(i);
+    }
+    return years;
+  }
+
+  private createMonths() : string[]{
+    return [
+      'Januari',
+      'Februari',
+      'Mars',
+      'April',
+      'Maj',
+      'Juni',
+      'Juli',
+      'Augusti',
+      'September',
+      'Oktober',
+      'November',
+      'December'
+    ];
+  }
+
+  private getIdForMonth(month : string) : number{
+    for(let i = 0; i < this.months.length; i++){
+      if(month === this.months[i]){
+        return i;
+      }
+    }
+    return -1;
+  }
 }
